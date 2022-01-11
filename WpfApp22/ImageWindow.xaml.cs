9bd847fe3cd9a2244959017a74f17f845b10591b
hideru <jota.16.stigma.world@gmail.com>
2022-01-11 12:04:23 +0900

    close all code

---
+++ WpfApp22/ImageWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WpfApp22
+{
+    /// <summary>
+    /// Interaction logic for ImageWindow.xaml
+    /// </summary>
+    public partial class ImageWindow : Window
+    {
+        public ImageWindow()
+        {
+            InitializeComponent();
+        }
+
+        // private void OpenImageFile()
+        // {
+        //     var dlg = new OpenFileDialog();
+        //     dlg.Title = "Select an image";
+        //     dlg.Filter = "Image file|*.gif;*.png;*.jpeg;*.jpg;*.bmp|All files|*.*";
+        //
+        //     if (dlg.ShowDialog() == true)
+        //     {
+        //         var window = new ImageWindow();
+        //         if (window.SetImage(dlg.FileName))
+        //         {
+        //             window.Show();
+        //         }
+        //     }
+        // }
+
+        public bool SetImage(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                return false;
+            }
+
+            var bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.UriSource = new Uri(filePath);
+            bmp.EndInit();
+            img.Source = bmp;
+            this.Title = System.IO.Path.GetFileName(filePath);
+
+            return true;
+        }
+
+        private void ChangeWindowSize(bool isZoom)
+        {
+            int plusMinus = isZoom ? 1 : -1;
+
+            this.Height += (this.Height * 0.1) * plusMinus;
+            this.Width += (this.Width * 0.1) * plusMinus;
+
+            if (this.Height < MinHeight || this.Width < MinWidth)
+            {
+                this.Height = MinHeight;
+                this.Width = MinWidth;
+            }
+        }
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ChangeWindowSize(e.Delta > 0);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void plusButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeWindowSize(true);
+        }
+
+        private void minusButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeWindowSize(false);
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- WpfApp22/MainWindow.xaml.cs
+++ WpfApp22/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WpfApp22
 {
@@ -23,6 +24,51 @@ namespace WpfApp22
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void OpenImageFile()
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Title = "Select an image";
+            dlg.Filter = "Image file|*.gif;*.png;*.jpeg;*.jpg;*.bmp|All files|*.*";
+
+            if (dlg.ShowDialog() == true)
+            {
+                var window = new ImageWindow();
+                if (window.SetImage(dlg.FileName))
+                // if (window.SetBinding(dlg.FileName))
+                {
+                    window.Show();
+                }
+            }
+        }
+
+        private void Open_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenImageFile();
+        }
+
+        private void CloseAll_OnClick(object sender, RoutedEventArgs e)
+        {
+            CloseSubWindows();
+        }
+
+        private void CloseSubWindows()
+        {
+            foreach (Window w in App.Current.Windows)
+            {
+                if (w != this)
+                {
+                    w.Close();
+                }
+            }
+        }
+        
+        private void
+
+        private void MainWindow_OnClosed(object sender, EventArgs e)
+        {
+            CloseSubWindows();
         }
     }
 }
